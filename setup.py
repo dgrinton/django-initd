69e0cceb367abb474f3601782d0767fc34ac9e49
Mateusz Barszcz <m@barsz.cz>
2016-08-17 14:22:37 +0200

    Fix Django 1.9 compatibility

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ doclines = __doc__.splitlines()
 
 setup(
     name = 'django-initd',
-    version = '0.0.2',
+    version = '0.0.2dev1',
     py_modules = ['initd', 'daemon_command'],
     platforms = ['POSIX'],
 
